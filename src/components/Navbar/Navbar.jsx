a0f3227bcc6270484aeade0682322caae07d5440
Juan1426 <juancruzzcaro1@gmail.com>
2023-05-24 18:48:08 -0300

    modified searcher

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,21 +2,10 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import { searchPokemon } from "../../api";
+
 const Navbar = () =>{
 
-  const [search, setSearch] = useState("")
-    const [pokemon, setPokemon] = useState()
-    
-    const onChange = (e) => {
-        console.log(e.target.value)
-        setSearch(e.target.value)  
-    }
-
-    const onClick = async (e) => {
-        const data = await searchPokemon(search)
-        console.log(data)
-        setPokemon(data)
-    }
+  
 
     return(
       <>
@@ -25,39 +14,7 @@ const Navbar = () =>{
               <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="logo Pokemon" />
             </div>
 
-            <form className="searchbar">
-              <div className="form-group">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="icon-search"
-                >
-                  <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                  />
-                </svg>
-                  <input 
-                      className="searcher"
-                      placeholder="Buscar pokemon..."
-                      onChange={onChange}
-                  />
-              </div>
-                  <button onClick={onClick} className="btn-search">Buscar</button>        
-             {/*<div>            
-                  {pokemon &&
-                    <div>
-                        <div>Nombre: {pokemon.name}</div>       
-                        <div>Peso: {pokemon.weight} kg</div>        
-                        <img src={pokemon.sprites.front_default} alt="" />
-                    </div>
-                  }                  
-              </div>*/}          
-            </form>
+            
         </header>
       </>
     )
--- src/pages/Pokedex/Pokedex.jsx
+++ src/pages/Pokedex/Pokedex.jsx
@@ -12,63 +12,107 @@ const Pokedex = () =>{
   const parametros = useParams()
   console.log(parametros)
 
-    const [pokemons, setPokemons] = useState([])
-    const [page, setPage] = useState(0)
-    const [total, setTotal] = useState(0)
-    const [loading, setLoading] = useState(true)
-  
-    const fetchPokemons = async () => {
-      try{
-        setLoading(true)
-        const data = await getPokemons(20, 20*page)    
-        const promises = data.results.map( async(pokemon) => {
-          return await getPokemonData(pokemon.url)
-        })
-        const results = await Promise.all(promises)
-        setPokemons(results)
-        setLoading(false)
-        setTotal(Math.ceil(data.count /20))
-      } catch(err){}
-    }
-   
-    useEffect(() => {   
-      fetchPokemons()
-    }, [page])
+  //States
+  const [pokemons, setPokemons] = useState([])
+  const [page, setPage] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [loading, setLoading] = useState(true)
+
+  //Searcher
+  const [search, setSearch] = useState([])
+    const [pokemon, setPokemon] = useState()
     
-    const lastPage =()=> {
-      const nextPage = Math.max(page - 1, 0)
-      setPage(nextPage)
+    const onChange = (e) => {
+        console.log(e.target.value)
+        setSearch(e.target.value)  
     }
 
-    const nextPage =()=> {
-      const nextPage = Math.min(page + 1, total)
-      setPage(nextPage)
+    const onClick = async (e) => {
+        const data = await searchPokemon(search)
+        console.log(data)
+        setPokemon(data)
     }
-    
-    return(
-      <main>
-          <Pagination 
-            page={page + 1}
-            totalPages={total}
-            onLeftClick = {lastPage}
-            onRightClick = {nextPage}
-          />
-          { loading 
-            ? <Loading />
-            : <div className="pokedex-grid">
-              
-              {
-                  pokemons.map((pokemon, idx) =>{
-                      return (
-                          <PokemonCard pokemon={pokemon} key={pokemon.name} />                        
-                      )
-                  })
-              }             
-          </div> 
-          }
-      </main>
 
-    )
+  //
+  const fetchPokemons = async () => {
+    try{
+      setLoading(true)
+      const data = await getPokemons(20, 20*page)    
+      const promises = data.results.map( async(pokemon) => {
+        return await getPokemonData(pokemon.url)
+      })
+      const results = await Promise.all(promises)
+      setPokemons(results)
+      setLoading(false)
+      setTotal(Math.ceil(data.count /20))
+    } catch(err){}
+  }
+
+
+  //Pagination
+  useEffect(() => {   
+    fetchPokemons()
+  }, [page])
+
+  const lastPage =()=> {
+    const nextPage = Math.max(page - 1, 0)
+    setPage(nextPage)
+  }
+
+  const nextPage =()=> {
+    const nextPage = Math.min(page + 1, total)
+    setPage(nextPage)
+  }
+    
+
+  return(
+    <main>
+      <form className="searchbar">
+        <div className="form-group">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="icon-search"
+          >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+            <input 
+                className="searcher"
+                placeholder="Buscar pokemon..."
+                onChange={onChange}
+            />
+        </div>
+            {/*<button onClick={onClick} className="btn-search">Buscar</button>*/}                      
+      </form>
+
+        <Pagination 
+          page={page + 1}
+          totalPages={total}
+          onLeftClick = {lastPage}
+          onRightClick = {nextPage}
+        />
+        { loading 
+          ? <Loading />
+          : <div className="pokedex-grid">
+            
+            {
+                pokemons.map((pokemon, idx) =>{
+                    return (
+                        <PokemonCard pokemon={pokemon} key={pokemon.name} />                        
+                    )
+                })
+            }             
+        </div> 
+        }
+    </main>
+  )
 }
 
 export default Pokedex
